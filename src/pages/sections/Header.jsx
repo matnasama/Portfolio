818931a8c7444ad3df9d551a6caa47f1a5182680
matnasama <110042114+matnasama@users.noreply.github.com>
2023-12-15 16:00:21 -0300

    15/12

--- src/pages/sections/Header.jsx
+++ src/pages/sections/Header.jsx
@@ -17,15 +17,15 @@ export default function Header () {
                   <Stack
                     direction="row"
                     divider={<Divider orientation="vertical" flexItem />}
-                    spacing={3}
+                    spacing={4}
                   >
-                    <HomeIcon className='icon-home' fontSize="medium" sx={{color:"#fff", marginRight:0, paddingLeft:4}}><Link to='/'></Link></HomeIcon>
+                    <HomeIcon className='icon-home' fontSize="medium" sx={{color:"#fff"}}><Link to='/'></Link></HomeIcon>
                     <Link to='/about'>play</Link>
                     <Link to='/tramites'>inventory</Link>
                     <Link to='/'>watch</Link>
                     <Link to='/about'>awards</Link>
                     <Link to='/about'>options</Link>
-                    <PowerSettingsNewIcon className='icon-end' fontSize="medium" sx={{color:"#fff", marginLeft:0, paddingRight:4}}><Link to='/'></Link></PowerSettingsNewIcon>
+                    <PowerSettingsNewIcon className='icon-end' fontSize="medium" sx={{color:"#fff"}}><Link to='/'></Link></PowerSettingsNewIcon>
                   </Stack>
 
                 </Box>
